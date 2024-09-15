66f989287e77fd862e65ecc71e1e27ec84c58f26
amel.chabah <amel.chabah@gael.fr>
2024-09-15 21:16:02 +0200

    fix/main apos

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
+/* eslint-disable react/no-unescaped-entities */
 
 export default function Signup() {
     const [email, setEmail] = useState('');
